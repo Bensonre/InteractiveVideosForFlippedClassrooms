a52a38860b7adad1721563d03d2cc85e4b6b9176
Devin <devinsather@live.com>
2020-01-28 23:38:42 -0800

    Querying the database correctly

--- src/controllers/QuestionController.php
+++ src/controllers/QuestionController.php
@@ -1,78 +1,3 @@
-  <!--
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Flipped Classroom</title>
-	  <link rel="stylesheet" href="../css/Site-Style.css"> 
-	</head>
-	<body>
-
-	<?php
-  /*
-		include "../includes/header.php";
-
-		$question = $_POST['question'];
-		$category = $_POST['category'];
-		$c1       = $_POST['a1'];
-		$c2       = $_POST['a2'];
-		$c3       = $_POST['a3'];
-		$c4       = $_POST['a4'];
-		$correct  = $_POST['correct'];
-
-		$choices = [$c1 => 1, 
-								$c2 => 2, 
-								$c3 => 3, 
-								$c4 => 4];
-
-		include "../includes/connectvars.php";
-
-	  $date = mysqli_query($mysqli, "SELECT CURDATE()")
-		or die('Error Querying the Database');
-		
-		//Insert values into questions table
-		$qquery = "INSERT INTO questions (QuestionText, Catagory, DateModified) " .
-						 "VALUES ('$question', '$category', CURDATE() )";
-
-		if(mysqli_query($mysqli, $qquery)) {
-			$last_id = mysqli_insert_id($mysqli);
-		}else{
-			die('Error Querying the Database');
-		}
-
-
-		foreach( $choices as $choice => $ord ) {
-			if( $ord == $correct ) {
-				$cquery = "INSERT INTO choices (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
-								 "VALUES ('$last_id', '$choice', '$ord', CURDATE(), TRUE )";
-				mysqli_query($mysqli, $cquery)
-				or die('Error Querying the Database');
-			} else {
-				$cquery = "INSERT INTO choices (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
-								 "VALUES ('$last_id', '$choice', '$ord', CURDATE(), FALSE )";
-				mysqli_query($mysqli, $cquery)
-				or die('Error Querying the Database');
-			}
-		}
-
-		mysqli_close($mysqli);
-		
-
-
-		echo "<p>Success! Your question has been added.</p>";
-		echo "<p>Question: $question</p>";
-		echo "<p>Category: $category</p>";
-		echo "<p>Choice 1: $c1</p>";
-		echo "<p>Choice 2: $c2</p>";
-		echo "<p>Choice 3: $c3</p>";
-		echo "<p>Choice 4: $c4</p>";
-    */
-	?>
-	</body>
-</html>
-*/
--->
-
-
 <?php
 class CreateQuestionController {
   private $conn;
@@ -83,64 +8,75 @@ class CreateQuestionController {
   private $choice3;
   private $choice4;
   private $correct;
-  private $qtable = 'question';
+  private $qtable = 'questions';
   private $ctable = 'choices';
+  private $last_id; 
+  private $choices;
 
-  private function InsertHelper($q)
-  {
-    $query = $this->conn->prepare($q);
-    if ($query == false) {
-      $error = $this->conn->errno . ' ' . $this->conn->error;
-      echo $error;
-    } else {
-      return $query->execute();
-    }
-  }
-
-
-  public function __construct($db, $category, $question, $choice1, $choice2, $choice3, $choice4, $correct)
+  public function __construct($db, $question, $category, $choice1, $choice2, $choice3, $choice4, $correct)
   {
     $this->conn = $db;
-    $this->quesiton = $question;
+    $this->question = $question;
     $this->category = $category;
     $this->choice1 = $choice1;
     $this->choice2 = $choice2;
     $this->choice3 = $choice3;
     $this->choice4 = $choice4;
+    $this->correct = $correct;
   }
 
   public function create()
   {
-    $this->question = htmlspecialchars(strip_tags($this->instructorId));
+    $this->question = htmlspecialchars(strip_tags($this->question));
     $this->category = htmlspecialchars(strip_tags($this->category));
-    $this->choice1 = htmlspecialchars(strip_tags($this->choice1);
-    $this->choice2 = htmlspecialchars(strip_tags($this->choice2);
-    $this->choice3 = htmlspecialchars(strip_tags($this->choice3);
-    $this->choice4 = htmlspecialchars(strip_tags($this->choice4);
-    $this->correct = htmlspecialchars(strip_tags($this->correct);
+    $this->choice1 = htmlspecialchars(strip_tags($this->choice1));
+    $this->choice2 = htmlspecialchars(strip_tags($this->choice2));
+    $this->choice3 = htmlspecialchars(strip_tags($this->choice3));
+    $this->choice4 = htmlspecialchars(strip_tags($this->choice4));
+    $this->correct = htmlspecialchars(strip_tags($this->correct));
 
-    $choices = [$this->choice1 => 1,
-                $this->chioce2 => 2,
-                $this->chioce3 => 3,
-                $this->chioce4 => 4]
+    $querySuccess1 = false;
+    $querySuccess2 = false;
 
-		$qquery = "INSERT INTO $this->qtable (QuestionText, Catagory, DateModified) VALUE ('$this->question', '$this->category',CURDATE() )";
+
+    $this->choices = [$this->choice1 => 1,
+                      $this->choice2 => 2,
+                      $this->choice3 => 3,
+                      $this->choice4 => 4];
+
+    $qquery = "INSERT INTO $this->qtable (QuestionText, Catagory, DateModified) VALUES (?, ?, CURDATE() )";
     
-    InsertHelper($qquery);
+    $query = $this->conn->prepare($qquery);
+    if ($query == false) {
+      $error = $this->conn->errno . ' ' . $this->conn->error;
+      echo $error;
+    } else {
+      $query->bind_param("ss", $this->question, $this->category);  
+      $query->execute();
+    }
 
-    $last_id = mysqli_insert_id($this->conn);
+    $this->last_id = mysqli_insert_id($this->conn);
     
-		foreach( $choices as $choice => $ord ) {
-			if( $ord == $this->correct ) {
-				$cquery = "INSERT INTO choices (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
-								 "VALUES ('$last_id', '$choice', '$ord', CURDATE(), TRUE )";
+    foreach( $this->choices as $choice => $ord ) {
+      if( $ord == $this->correct ) {
+        $cquery = "INSERT INTO $this->ctable (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
+                 "VALUES (?, ?, ?, CURDATE(), TRUE )";
 
-			} else {
-				$cquery = "INSERT INTO choices (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
-								 "VALUES ('$last_id', '$choice', '$ord', CURDATE(), FALSE)";
-			}
-        InsertHelper($cquery);
-		}
+      } else {
+        $cquery = "INSERT INTO $this->ctable (QuestionID, ChoiceText, ChoiceOrder, DateModified, correct) " .
+                 "VALUES (?, ?, ?, CURDATE(), FALSE)";
+      }
+      
+      $query = $this->conn->prepare($cquery);
+      if ($query == false) {
+        $error = $this->conn->errno . ' ' . $this->conn->error;
+        echo $error;
+      } else {
+        $query->bind_param("isi", $this->last_id, $choice, $ord);  
+        $query->execute();
+      }
+    }
+    return true;
     
   }
 }
