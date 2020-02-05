d6f00ce7f7fe221ae90c211ac3079775a109a54f
orourtim <orourtim@oregonstate.edu>
2020-02-05 00:50:07 -0800

    Answer create API page.

---
+++ src/api/answers/create.php
@@ -0,0 +1,24 @@
+<?php
+    include_once '../../database/Database.php';
+    include_once '../../controllers/AnswerController.php';
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $controller = new AnswerController($db);
+
+    if(!empty($_POST['questionID']) && !empty($_POST['choiceID']) && !empty($_POST['studentID']) ) {
+        $success = $controller->create($_POST['questionID'], $_POST['choiceID'], $_POST['studentID']);
+        if ($success) {
+            echo "Answer submitted.";
+        }
+    }
+
+    echo "Answer was not successfully submitted.";
+?>
