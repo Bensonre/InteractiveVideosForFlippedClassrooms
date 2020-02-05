2c981727583740f6748b749b85bc5f990c31a7dd
Reese <Bensonre@oregonstate.edu>
2020-02-05 02:27:40 -0800

    feat: added create delete and read methods to package

--- src/controllers/PackagesController.php
+++ src/controllers/PackagesController.php
@@ -7,10 +7,27 @@ class PackageController {
     private $QuestionsTable = 'questions';
     private $PackageTable = 'packages';
     private $VideoTable = 'videos';
-
+    private $Date = null;
+    private $Title = null;
+    
     public function __construct($db)
     {
         $this->conn = $db;
+    }
+
+    public function create(){
+        $this->Date = htmlspecialchars(strip_tags($this->Date));
+        $this->Title = htmlspecialchars(strip_tags($this->Title));
+
+        $query = "INSERT INTO $this->PackageTable (`Date`, `Title`) VALUES (?,?)";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt == false) {
+            $error = $this->conn->errno . ' ' . $this->conn->error;
+            echo $error;
+        } else {
+            $stmt->bind_param("ss", $this->Title, $this->Date);
+            return $stmt->execute();
+        }
     }
 
     public function getPackageWithVideo($id)
