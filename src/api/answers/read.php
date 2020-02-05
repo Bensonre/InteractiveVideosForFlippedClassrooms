40360a69a907cd9da6a86f6887348e84b5b4b98a
orourtim <orourtim@oregonstate.edu>
2020-02-05 00:53:26 -0800

    Answer read API page.

---
+++ src/api/answers/read.php
@@ -0,0 +1,24 @@
+<?php
+    include_once '../../database/Database.php';
+    include_once '../../controllers/AnswerController.php';
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $database = new Database();
+    $db = $database->connect();
+
+    $controller = new AnswerController($db);
+
+    if(!empty($_GET['questionID']) && !empty($_GET['studentID']) ) {
+        $result = $controller->read($_GET['questionID'], $_GET['studentID']);
+        if ($result != null) {
+            echo json_encode($result);
+        }
+    }
+
+    echo "There is an error with the request.";
+?>
