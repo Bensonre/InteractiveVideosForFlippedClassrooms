d2c60f1722ac6c1601e51d6ea7483e16d6f0b778
orourtim <orourtim@oregonstate.edu>
2020-02-04 19:55:28 -0800

    AnswerController create() implemented.

--- src/controllers/AnswerController.php
+++ src/controllers/AnswerController.php
@@ -9,7 +9,20 @@ class AnswerController {
     }
 
     public function create($questionID, $choiceID, $studentID) {
+        $questionID = htmlspecialchars(strip_tags($questionID));
+        $choiceID = htmlspecialchars(strip_tags($choiceID));
+        $studentID = htmlspecialchars(strip_tags($studentID));
 
+        $query = "INSERT INTO $this->table (`QuestionID`, `ChoiceID`, `StudentID`, `AnswerDate`) VALUES (?,?,?,CURDATE())";
+        $stmt = $this->conn->prepare($query);
+        if ($stmt == false) {
+            $error = $this->conn->errno . ' ' . $this->conn->error;
+            echo $error;
+        } else {
+            $stmt->bind_param("iii", $questionID, $choiceID, $studentID);
+            return $stmt->execute();
+        }
+        return false;
     }
 
     public function read($questionID, $studentID) {
