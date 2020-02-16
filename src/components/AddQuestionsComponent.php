f6d167d89a7f21700af3f9d5cc7d63a5776f5e3f
orourtim <orourtim@oregonstate.edu>
2020-02-15 17:54:24 -0800

    Made things prettier using Bootstrap 4.

--- src/components/AddQuestionsComponent.php
+++ src/components/AddQuestionsComponent.php
@@ -1,42 +1,44 @@
 <link href="../css/ivc.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <link href="https://vjs.zencdn.net/7.6.6/video-js.css" rel="stylesheet" />
 <link href="../dependencies/dist/videojs.markers.css" rel="stylesheet">
 
-<div class="ivc-container">
-    <div class="section-title">Add Questions to a Package</div>
-    <div class="quarter-width" id="ivc-add-questions-form-wrapper">
-        <form>
-            <div class="label">Select a Package</div>
-            <select name="select-package" id="select-package" onchange="packageChanged()"></select>
-            <br />
-            <br />
-            <div class="label">Select a Question</div>
-            <select name="select-question" id="select-question"></select>
-            <br />
-            <br />
-            <div class="label">Time Stamp</div>
-            <input name="timestamp" id="TimeStamp" onkeyup="updateVideoTime()" type="string" />
-            <br />
-            <br />
-            <button onclick="sendData()" type="button" class="ivc-button-positive">Add to Package</button>
-        </form>
-        <div id="ivc-add-questions-status-message"></div>
+<div class="container">
+    <div class="row">
+        <div class="col m-4 border border-dark">
+            <h2 class="text-center">Add Questions to a Package</h2>
+            <form>
+                <div class="form-group">
+                    <div class="label" for="select-package">Select a Package</div>
+                    <select class="form-control" name="select-package" id="select-package" onchange="packageChanged()"></select>
+                </div>
+                <div class="form-group">
+                    <div class="label" for="select-question">Select a Question</div>
+                    <select class="form-control" name="select-question" id="select-question"></select>
+                </div>
+                <div class="form-group">
+                    <div class="label" for="TimeStamp">Timestamp</div>
+                    <input class="form-control" name="timestamp" id="TimeStamp" onkeyup="updateVideoTime()" type="string" /> 
+                </div>
+                <button class="form-control mb-3 btn btn-primary" onclick="sendData()" type="button" class="">Add to Package</button>
+            </form>
+            <div class="text-center" id="ivc-add-questions-status-message"></div>
+        </div>
+        <div class="col m-4 border border-dark">
+            <div class="mt-4 mb-4" style="height: 50vh; overflow: auto;">
+            <table class="table table-striped table-bordered table-sm" id="ivc-add-questions-added-table"></table>
+            </div>
+        </div>
+        <div class="col m-4 border border-dark">
+            <video-js
+            id="AddQuestions-video"
+            controls 
+            autoplay
+            data-setup="{}"
+		        >
+            </video-js>
+        </div>
     </div>
-
-    <div class="quarter-width" id="ivc-add-questions-added-table-wrapper">
-        <table id="ivc-add-questions-added-table"></table>
-    </div>
-				
-    <div class="half-width" id="ivc-add-questions-player-wrapper">
-        <video-js
-        id="AddQuestions-video"
-        controls 
-        autoplay
-        data-setup="{}"
-		    >
-        </video-js>
-    </div>
-
 </div> <!-- >End IVC Container <!-->
 
 <script src="../js/UpdateVideoTime.js"></script>
