827f74505c1844d0db549956ebdbe9396901fcb4
orourtim <orourtim@oregonstate.edu>
2020-02-18 14:37:54 -0800

    Few Bootstrap changes to more closely resemble the other components.

--- src/components/PackageComponent.php
+++ src/components/PackageComponent.php
@@ -14,49 +14,50 @@
 
     <div class="tab-content">
         <div id="ivc-create-package-form" class="tab-pane container active">
-            <h2 class="text-center">Create Package</h2>
-            <form method="Post" action="../api/Packages/create.php">
-            <div class="label">Select a video</div>
-            <select id="create-package-select-video" Name="VideoId" onchange="updateCreatePackageVideoFilePath()" class="PushLeft1">
-            <?php
-                    foreach($videoResponse as $video){
-                        echo "<option video-path=\"";
-                        if(!array_key_exists('filePath', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['filePath'];
-                         }
-                        echo "\" value=";
-                        if(!array_key_exists('id', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['id'];
-                         }
-                        echo ">";
-                        if(!array_key_exists('title', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['title'];
-                         }
-                        echo "</option>";
-                    }
-                ?>
-            </select>
-            <div class="label">
-                Title the Package
-            </div>
-            <input name="Title" placeholder="Package Name" Type="text" class="PushLeft1"/>
+            <h2 class="text-center">Create Packages</h2>
+            <form method="post" action="../api/Packages/create.php">
+                <div class="form-group">
+                    <label>Select a video</label>
+                    <select id="create-package-select-video" Name="VideoId" onchange="updateCreatePackageVideoFilePath()" class="form-control">
+                    <?php
+                            foreach($videoResponse as $video){
+                                echo "<option video-path=\"";
+                                if(!array_key_exists('filePath', $video)){
+                                    echo"Invlaid Package";
+                                }else{
+                                    echo $video['filePath'];
+                                }
+                                echo "\" value=";
+                                if(!array_key_exists('id', $video)){
+                                    echo"Invlaid Package";
+                                }else{
+                                    echo $video['id'];
+                                }
+                                echo ">";
+                                if(!array_key_exists('title', $video)){
+                                    echo"Invlaid Package";
+                                }else{
+                                    echo $video['title'];
+                                }
+                                echo "</option>";
+                            }
+                        ?>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label>Title</label>
+                    <input name="Title" placeholder="Name the package..." Type="text" class="form-control"/>
+                </div>
             <input name="InstructorID" Type="hidden" value=<?php echo "\""; echo $instructorID; echo "\""; ?>/>
-            <label class="label">
-                Selected Video
-            </label>
+            <label>Selected video</label>
             <video-js
                 id="Create-Package-video"
                 controls 
                 data-setup="{}">
                 <source id="create-video-source" src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
             </video-js>
-            <button class="form-control mt-3 btn btn-primary" type="Submit">Submit</button>
+            <button class="form-control mt-3 btn btn-primary" type="Submit">Create</button>
             
         </form>  
         </div>
@@ -64,64 +65,70 @@
         <div id="ivc-update-package-form" class="tab-pane container fade">
             <h2 class="text-center">Update Packages</h2>
             <form method="Post" action="../api/Packages/Update.php"> 
-            <div class="label">Select an exsisting package</div>
-            <select id="update-package-selection" class="PushLeft1" name="ID" onchange="updateUpdatePackageOnNewPackageSelected()">
+            <div class="form-group">
+                <label>Select an existing package</label>
+                <select id="update-package-selection" class="form-control" name="ID" onchange="updateUpdatePackageOnNewPackageSelected()">
+                    <?php
+                        foreach($packageResponse as $package){
+                            echo "<option video-id=\"";
+                            if(!array_key_exists('videoId', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['videoId'];
+                            }
+                            echo "\" value=";
+                            if(!array_key_exists('id', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['id'];
+                            }
+                            echo ">";
+                            if(!array_key_exists('title', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['title'];
+                            }
+                            echo "</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Title</label>
+                <input id="update-package-title" name="Title" placeholder="Name the package..." Type="text" class="form-control"/>
+            </div>
+
+            <div class="form-group">
+                <label>Select new video</label>
+                <select id="update-package-select-video" name="VideoID" class="form-control" onchange="updateUpdatePackageVideoFilePath()">
                 <?php
-                    foreach($packageResponse as $package){
-                        echo "<option video-id=\"";
-                        if(!array_key_exists('videoId', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['videoId'];
+                        foreach($videoResponse as $video){
+                            echo "<option video-path=\"";
+                            if(!array_key_exists('filePath', $video)){
+                                echo"Invlaid Package";
+                            }else{
+                                echo $video['filePath'];
+                            }
+                            echo "\" value=";
+                            if(!array_key_exists('id', $video)){
+                                echo"Invlaid Package";
+                            }else{
+                                echo $video['id'];
+                            }
+                            echo ">";
+                            if(!array_key_exists('title', $video)){
+                                echo"Invlaid Package";
+                            }else{
+                                echo $video['title'];
+                            }
+                            echo "</option>";
                         }
-                        echo "\" value=";
-                        if(!array_key_exists('id', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['id'];
-                         }
-                        echo ">";
-                        if(!array_key_exists('title', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['title'];
-                         }
-                        echo "</option>";
-                    }
-                ?>
-            </select>
-            <label class="label"> Package Title </label>
-            <input id="update-package-title" name="Title" placeholder="Package Name" Type="text" class="PushLeft1"/>
-            <div class="label">Select New video</div>
-            <select id="update-package-select-video" name="VideoID" class="PushLeft1" onchange="updateUpdatePackageVideoFilePath()">
-            <?php
-                    foreach($videoResponse as $video){
-                        echo "<option video-path=\"";
-                        if(!array_key_exists('filePath', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['filePath'];
-                        }
-                        echo "\" value=";
-                        if(!array_key_exists('id', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['id'];
-                         }
-                        echo ">";
-                        if(!array_key_exists('title', $video)){
-                            echo"Invlaid Package";
-                        }else{
-                            echo $video['title'];
-                         }
-                        echo "</option>";
-                    }
-                ?>
-            </select>
+                    ?>
+                </select>
+            </div>
             <input name="InstructorID" Type="hidden" value=<?php echo "\""; echo $instructorID; echo "\""; ?>/>
-            <label class="label">
-                Selected Video
-            </label>
+            <label>Selected video</label>
             <video-js
                 id="Update-Package-video"
                 controls 
@@ -129,39 +136,41 @@
                 <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
             </video-js>
             
-            <button class="form-control mt-3 btn btn-primary" type="Submit">Update</button>
+            <button class="form-control mt-3 btn btn-warning" type="Submit">Update</button>
             </form>
         </div>
 
         <div id="ivc-delete-package-form" class="tab-pane container fade">
             <h2 class="text-center">Delete Packages</h2>
             <form method="Post" action="../api/Packages/delete.php"> 
-                <div class="label">Select a Package to delete</div>
-                <select class="PushLeft1" name="id">
-                <?php
-                    foreach($packageResponse as $package){
-                        echo "<option video-id=\"";
-                        if(!array_key_exists('videoId', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['videoId'];
+                <div class="form-group">
+                    <label>Select a package to delete</label>
+                    <select class="form-control" name="id">
+                    <?php
+                        foreach($packageResponse as $package){
+                            echo "<option video-id=\"";
+                            if(!array_key_exists('videoId', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['videoId'];
+                            }
+                            echo "\" value=";
+                            if(!array_key_exists('id', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['id'];
+                            }
+                            echo ">";
+                            if(!array_key_exists('title', $package)){
+                                echo"error";
+                            }else{
+                                echo $package['title'];
+                            }
+                            echo "</option>";
                         }
-                        echo "\" value=";
-                        if(!array_key_exists('id', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['id'];
-                         }
-                        echo ">";
-                        if(!array_key_exists('title', $package)){
-                            echo"error";
-                        }else{
-                            echo $package['title'];
-                         }
-                        echo "</option>";
-                    }
-                ?>
-                </select>
+                    ?>
+                    </select>
+                </div>
                 <input name="ID" type="hidden"/>
                 <button class="form-control mt-3 btn btn-danger" type="Submit">Delete</button>
             </form>
