b5b16e3ea16ad8c2f52106cf44649a2c5645a349
orourtim <orourtim@oregonstate.edu>
2020-02-18 16:11:15 -0800

    Moved the player to the right of the form on the update tab.

--- src/components/PackageComponent.php
+++ src/components/PackageComponent.php
@@ -46,34 +46,43 @@
 
         <div id="ivc-update-package-form" class="tab-pane container fade">
             <h2 class="text-center">Update Packages</h2>
-            <form> 
-                <div class="form-group">
-                    <label>Select an existing package</label>
-                    <select id="update-package-selection" class="form-control" name="ID" onchange="updateUpdatePackageOnNewPackageSelected()"></select>
+            <div class="row">
+                <div class="col">
+                    <form> 
+                        <div class="form-group">
+                            <label>Select an existing package</label>
+                            <select id="update-package-selection" class="form-control" name="ID" onchange="updateUpdatePackageOnNewPackageSelected()"></select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input id="update-package-title" name="Title" placeholder="Name the package..." Type="text" class="form-control"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Select new video</label>
+                            <select id="update-package-select-video" name="VideoID" class="form-control" onchange="updateUpdatePackageVideoFilePath()"></select>
+                        </div>
+
+                        <input name="InstructorID" Type="hidden" value=<?php echo "\""; echo $instructorID; echo "\""; ?>/>
+                        
+                    
+                        <button class="form-control mt-3 btn btn-warning" type="button" onclick="updatePackage()">Update</button>
+                        <div id="ivc-update-package-status-message" class="text-center"></div>
+                    </form>
                 </div>
 
-                <div class="form-group">
-                    <label>Title</label>
-                    <input id="update-package-title" name="Title" placeholder="Name the package..." Type="text" class="form-control"/>
+                <div class="col">
+                    <label>Selected video</label>
+                    <video-js
+                        id="Update-Package-video"
+                        class="vjs-16-9"
+                        controls 
+                        data-setup="{}">
+                        <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
+                    </video-js>
                 </div>
-
-                <div class="form-group">
-                    <label>Select new video</label>
-                    <select id="update-package-select-video" name="VideoID" class="form-control" onchange="updateUpdatePackageVideoFilePath()"></select>
-                </div>
-
-                <input name="InstructorID" Type="hidden" value=<?php echo "\""; echo $instructorID; echo "\""; ?>/>
-                <label>Selected video</label>
-                <video-js
-                    id="Update-Package-video"
-                    controls 
-                    data-setup="{}">
-                    <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
-                </video-js>
-            
-                <button class="form-control mt-3 btn btn-warning" type="button" onclick="updatePackage()">Update</button>
-                <div id="ivc-update-package-status-message" class="text-center"></div>
-            </form>
+            </div>
         </div>
 
         <div id="ivc-delete-package-form" class="tab-pane container fade">
