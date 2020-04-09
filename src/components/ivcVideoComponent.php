dd25a6de226e9ebe6aa9f0f42c2ec63486915adc
orourtim <orourtim@oregonstate.edu>
2020-04-08 20:02:43 -0700

    Upload progress bar works.

--- src/components/ivcVideoComponent.php
+++ src/components/ivcVideoComponent.php
@@ -1,14 +1,16 @@
 <div class="container border border-dark p-4">
     <ul class="nav nav-pills">
-        <li class="nav-item"><a class="nav-link active" data-toggle="pill" href="#ivc-create-video-form">Create</a></li>
+        <li class="nav-item"><a class="nav-link active" data-toggle="pill" href="#ivc-create-video-form">Upload</a></li>
         <li class="nav-item"><a class="nav-link" data-toggle="pill" href="#ivc-update-video-form">Update</a></li>
-        <li class="nav-item"><a class="nav-link"data-toggle="pill" href="#ivc-delete-video-form">Delete</a></li>
+        <li class="nav-item"><a class="nav-link" data-toggle="pill" href="#ivc-delete-video-form">Delete</a></li>
     </ul>
 
     <div class="tab-content">
         <div id="ivc-create-video-form" class="tab-pane container active">
             <h2 class="text-center">Upload Videos</h2>
-            <form id="uvideoform">
+
+            <form id="uvideoform" method="POST" enctype="multipart/form-data" target="hidden-frame">
+                <input id="ivc-upload-progress" type="hidden" value="uvideoform" name="<?php echo ini_get("session.upload_progress.name"); ?>">
                 <div class="form-group">
                     <label>Video</label>
                     <input id="ivc-video-select-create" type="file" class="form-control-file" name="local-video-file" accept="video/*">
@@ -19,9 +21,24 @@
                     <input id="ivc-video-title-create" type="text" class="form-control" placeholder="Name the video..."></input>
                 </div>
 
-                <button class="form-control mt-3 btn btn-primary" type="button" onclick="createVideo()">Create</button>
+                <button class="form-control mt-3 btn btn-primary" type="submit" onclick="createVideo();">Upload</button>
+
+                <div class="row">
+                    <div class="col">
+                        <div id="ivc-progress-bar">
+                            <div id="ivc-progress-bar-color"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <div id="ivc-progress-bar-status"></div>
+                    </div>
+                </div>
+
                 <div id="ivc-create-video-status-message" class="text-center"></div>
             </form>
+            <iframe id="hidden-frame" name="hidden-frame" src="about:blank" hidden></iframe>
         </div>
 
         <div id="ivc-update-video-form" class="tab-pane container fade">
